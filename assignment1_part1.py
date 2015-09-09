7b0b61322fdf7c735363d49b21594d7c00470666
Harry Nunez <harry.nunez@spsmail.cuny.edu>
2015-09-08 21:03:54 -0400

    assignment1_part1.py

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,3 +1,27 @@
-Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+class ListDivideException(Exception):
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def listDivide(numbers, divide=2):
+
+    mylist = []
+    for i in numbers:
+        if i % divide == 0:
+            mylist.append(i)
+        return len(mylist)
+
+
+def testListDivide():
+
+    tests = [
+         ([1, 2, 3, 4, 5]),
+         ([2, 4, 6, 8, 10]),
+         ([30, 54, 63, 98, 100]),
+         ([]),
+         ([1, 2, 3, 4, 5])
+         ]
+for i in tests:
+            if listDivide != divide:
+                raise ListDivideException('Test Failed')
---
+++ assignment1_part2.py
@@ -0,0 +1,11 @@
+self.title = title 
+
+ 
+
+ 
+
+def display(self): 
+
+    print "{}, written by {}".format(self.title, self.author) 
+
+B1 = Book('John Steinbeck', 'Of Mice and Men')
